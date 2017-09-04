67a4869408d383e471edf02efa4bfcfcf60a495e
Dominik Kundel <dominik.kundel@gmail.com>
2017-09-04 15:34:03 +0200

    :fire: Clean up index.js for template

--- index.js
+++ index.js
@@ -6,29 +6,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/token', (req, res) => {
-  const ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
-  const SERVICE_SID = process.env.TWILIO_SYNC_SERVICE;
-  const API_KEY = process.env.TWILIO_API_KEY;
-  const API_SECRET = process.env.TWILIO_API_SECRET;
-
-  const IDENTITY = 'only for testing';
-
-  const AccessToken = Twilio.jwt.AccessToken;
-  const SyncGrant = AccessToken.SyncGrant;
-
-  const syncGrant = new SyncGrant({
-    serviceSid: SERVICE_SID
-  });
-
-  const accessToken = new AccessToken(ACCOUNT_SID, API_KEY, API_SECRET);
-
-  accessToken.addGrant(syncGrant);
-  accessToken.identity = IDENTITY;
-
-  res.send({ token: accessToken.toJwt() });
-});
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
